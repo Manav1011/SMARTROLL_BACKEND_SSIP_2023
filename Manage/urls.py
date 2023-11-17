86ca6aa655d1ae38d34fb75466dca78c01a2eeb6
Manav1011 <manavshah1011.ms@gmail.com>
2023-11-17 18:52:59 +0530

    API added: Get Subjects, Add Subjects

--- Manage/urls.py
+++ Manage/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import get_batches,add_batches,get_semesters,add_semester
+from .views import get_batches,add_batches,get_semesters,add_semester,get_subjects,add_subjects
 urlpatterns = [
     path('get_batches',get_batches,name='get_batches'),
     path('add_batch',add_batches,name='add_batch'),
     path('get_semesters',get_semesters,name='get_semesters'),
     path('add_semester',add_semester,name='add_semester'),
+    path('get_subjects',get_subjects,name='get_subjects'),
+    path('add_subjects',add_subjects,name='add_subjects'),
 ]
--- StakeHolders/views.py
+++ StakeHolders/views.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,    
     TokenRefreshView,
 )
-from .models import Admin
+from .models import Admin,Teacher
 from .serializers import AdminSerializer
 import json
 
@@ -22,6 +22,31 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
                 admin_obj = Admin.objects.get(profile=user)
                 admin_serializer = AdminSerializer(admin_obj,many=False)
                 # Admin exclusive fields will be here
+                branch_obj = admin_obj.branch
+                # Count the batches in particular branch - from branch
+                batch_obj = branch_obj.batches.all()
+                batches_count = batch_obj.count()
+                # Count semesters in each batches - from batches
+                semesters = []
+                for i in batch_obj:
+                    semesters_obj = i.semesters.all()
+                    for j in semesters_obj:
+                        semesters.append(j)
+                semesters_count = len(semesters)
+                # Count Subjects in each semesters - from semester
+                subjects = []
+                for i in semesters:
+                    subjects_obj = i.subjects.all()
+                    for j in subjects_obj:
+                        subjects.append(j)
+                subjects_count = len(subjects)
+                # Count teachers in the branch - from reverse query on branch
+                teachers = Teacher.objects.filter(branch=branch_obj)
+                teachers_count = teachers.count()
+                token['batches'] = batches_count
+                token['semesters'] = semesters_count
+                token['subjects'] = subjects_count
+                token['teachers'] = teachers_count
                 token['admin_obj'] = admin_serializer.data        
         return token
     
