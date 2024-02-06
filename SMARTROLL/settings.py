e434996d5cf954ca48a8667047659ae9f10a5a41
Manav1011 <manavshah1011.ms@gmail.com>
2024-02-06 22:34:02 +0530

    New branch

--- SMARTROLL/settings.py
+++ SMARTROLL/settings.py
@@ -125,23 +125,23 @@ ASGI_APPLICATION = "SMARTROLL.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smartroll',
-        'USER': 'postgres',
-        'PASSWORD': 'Manav@1011',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'smartroll',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Manav@1011',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
