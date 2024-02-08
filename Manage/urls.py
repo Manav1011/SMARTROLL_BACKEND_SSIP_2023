94fc098b7b5dbd78e8770aaf9f9d7369eeeb58fa
Harsh <parmarharsh901@gmail.com>
2024-02-08 14:55:27 +0530

    Add and Get subjects api added

--- Manage/urls.py
+++ Manage/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
-from .views import get_object_counts,add_semester,get_semesters,add_division
+from .views import get_object_counts,add_semester,get_semesters,add_division,get_subjects,add_subject
 urlpatterns = [        
     path('get_object_counts',get_object_counts,name='get_object_counts'),    
     path('add_semester/',add_semester,name='add_semester'),
     path('get_semesters/',get_semesters,name='get_semesters'),
     path('add_division/',add_division,name='add_division'),
+    path('get_subjects/',get_subjects,name='get_subjects'),
+    path('add_subject/',add_subject,name='add_subject'),
 ]
